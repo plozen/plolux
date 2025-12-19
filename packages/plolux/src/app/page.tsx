3517326a90a33c8a95935bb1056d18a6acf1ad0d
moony01 <mun01180@gmail.com>
2025-12-19 18:29:23 +0900

    fix(assets): Use static imports for images to resolve basePath automatically [deploy:plolux]

--- packages/plolux/src/app/page.tsx
+++ packages/plolux/src/app/page.tsx
@@ -1,4 +1,6 @@
 import Image from "next/image";
+import NextLogo from "../assets/next.svg";
+import VercelLogo from "../assets/vercel.svg";
 
 export default function Home() {
   return (
@@ -6,7 +8,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src="/next.svg"
+          src={NextLogo}
           alt="Next.js logo"
           width={100}
           height={20}
@@ -43,7 +45,7 @@ export default function Home() {
           >
             <Image
               className="dark:invert"
-              src="/vercel.svg"
+              src={VercelLogo}
               alt="Vercel logomark"
               width={16}
               height={16}
